2dd4285291fbc723418c30996934bdf13d2f1cab
Attila Szucs <attila.szucs@gmail.com>
2017-06-06 15:27:46 +0200

    mysql terminal small edit

--- mysql-terminal.sh
+++ mysql-terminal.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-docker run -it --rm --name mysqlterm --network debeziumtutorial_default --link mysql mysql:5.7 sh -c 'exec mysql -h"mysql" -P"3306" -uroot -p"debezium"'
+docker run -it --rm \
+  --name mysqlterm \
+  --network debeziumtutorial_default \
+  --link mysql mysql:5.7 \
+  sh -c 'exec mysql -h"mysql" -P"3306" -uroot -p"debezium"'
