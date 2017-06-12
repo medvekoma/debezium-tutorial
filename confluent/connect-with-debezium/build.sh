80a5e7403ca2565b62dccf5ca101f29d75c40c86
Attila Szucs <attila.szucs@gmail.com>
2017-06-12 12:03:25 +0200

    use confluent's containers

---
+++ confluent/connect-with-debezium/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t medvekoma/cp-connect-debezium .
+
---
+++ confluent/mysql-terminal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker run -it --rm \
+  --name mysqlterm \
+  --network confluent_default \
+  --link mysql mysql:5.7 \
+  sh -c 'exec mysql -h"mysql" -P"3306" -uroot -p"debezium"'
