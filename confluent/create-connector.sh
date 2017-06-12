9ec6907159f0cc19a6fb8b86dc2ed0efede3d013
Attila Szucs <attila.szucs@gmail.com>
2017-06-12 16:07:26 +0200

    debezium works in confluent

--- confluent/create-connector.sh
+++ confluent/create-connector.sh
@@ -15,7 +15,7 @@ curl -i -X POST \
       "database.server.id": "184054",
       "database.server.name": "dbserver1",
       "database.whitelist": "inventory",
-      "database.history.kafka.bootstrap.servers": "kafka:9092",
+      "database.history.kafka.bootstrap.servers": "broker:9092",
       "database.history.kafka.topic": "dbhistory.inventory"
     }
   }'
